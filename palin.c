f68d3c1e38b329a4e9f45032399f9d3f1df0aee9
15cs026priyanka <priyankaappu1998@gmail.com>
2018-07-23 12:25:08 +0530

    Create palin.c

---
+++ palin.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void isPalindrome(char str[])
+{
+       int l = 0;
+    int h = strlen(str) - 1;
+ 
+        while (h > l)
+    {
+        if (str[l++] != str[h--])
+        {
+            printf("%s is Not Palindrome", str);
+            return;
+        }
+    }
+    printf("%s is palindrome", str);
+}
+ 
